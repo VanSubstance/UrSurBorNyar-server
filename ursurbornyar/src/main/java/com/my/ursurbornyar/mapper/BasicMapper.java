34d222c1fe83f69256febb0f915850768b07c035
chener <49267211+chanosong@users.noreply.github.com>
2022-08-29 00:38:35 +0900

    add(Insert placelist function)
    #6
    
    + Insert placeList 기능 추가
    + Insert query 추가
    + UtilService 추가
    
    @VanSubstance

--- ursurbornyar/src/main/java/com/my/ursurbornyar/mapper/BasicMapper.java
+++ ursurbornyar/src/main/java/com/my/ursurbornyar/mapper/BasicMapper.java
@@ -3,11 +3,14 @@ package com.my.ursurbornyar.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-
+import com.my.ursurbornyar.vo.Place;
 import com.my.ursurbornyar.vo.Path;
 
 public interface BasicMapper {
 
+	public int insertPlace(@Param("place")Place place);
+	public int countPlace();
+	
 	public int check();
 	
 	public void insertUser(@Param("user") Path user);
---
+++ ursurbornyar/src/main/java/com/my/ursurbornyar/service/UtilService.java
@@ -0,0 +1,12 @@
+package com.my.ursurbornyar.service;
+
+import java.util.List;
+
+import com.my.ursurbornyar.vo.Place;
+import com.my.ursurbornyar.vo.Path;
+
+public interface UtilService {
+	
+	public String convertHex(int num);
+	public Boolean checkDuplicatedPlace(Place place);
+}
